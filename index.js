4c1d133c6f1c660cbadbc4470b4c731456d456c3
margaret manyeki <maggiemanyeki6@gmail.com>
2022-06-08 14:56:31 +0300

    commit tree a49a560f8d46feff7791aa16dce388398de1ab3a

---
+++ index.js
@@ -0,0 +1,10 @@
+
+ const divide =(num1=2000,num2=100)=> num1/num2;
+
+const square = (num=2)=> num**2;
+
+let add = (parameter1 , parameter2) =>{
+    console.log(`add ${parameter1}`);
+    console.log(`add ${parameter2}`);
+    return parameter1 + parameter2;
+}
